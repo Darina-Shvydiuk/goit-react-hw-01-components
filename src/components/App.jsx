06dc48ce2a6345507eed1cda9ed94a3d1cf0a4b0
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-09-05 17:38:45 +0300

    add 4 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { Profile } from './Profile/Profile';
 import user from './Profile/user';
+
 import { Statistics } from './Statistics/Statistics';
 import data from './Statistics/data';
+
 import { FriendList } from './FriendList/FriendList';
 import friends from './FriendList/friends';
-import { FriendListItem } from './FriendListItem/FriendListItem';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions';
 
 export const App = () => {
   return (
@@ -19,6 +23,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
